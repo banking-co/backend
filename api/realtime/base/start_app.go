c9c259b902b7ab432025e14f93f36c4ecac1d1fb
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-07-19 02:45:02 +0500

    working...

--- api/realtime/base/start_app.go
+++ api/realtime/base/start_app.go
@@ -2,9 +2,26 @@ package base
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"net"
+	"rabotyaga-go-backend/utils"
 )
 
 func StartApp(conn net.Conn, code ws.OpCode, data json.RawMessage) {
+	reqData, err := utils.UnmarshalData[interface{}](data)
+
+	if err == nil {
+		resData, err := utils.MarshalData(reqData)
+		if err != nil {
+			return
+		}
+
+		fmt.Println("send")
+		err = wsutil.WriteServerMessage(conn, code, resData)
+		if err != nil {
+			return
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -31,7 +31,7 @@ func Init() *Server {
 
 func (s *Server) Listen() {
 	err := http.ListenAndServe(":3001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Connection")
+		fmt.Printf("Connection at [%s] \r\n", time.Now())
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
 			log.Panicln("Upgrade HTTP error")
