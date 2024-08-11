7709c8eccb1a4ae63acb8cb8dcc0cbd46cdeb6e3
Dmitry Maximyuk <66935827+dmaximyuk@users.noreply.github.com>
2024-08-12 03:13:53 +0500

    working...

---
+++ entities/request.go
@@ -0,0 +1,66 @@
+package entities
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/SevereCloud/vksdk/v3/vkapps"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"net"
+	"rabotyaga-go-backend/dto"
+	"rabotyaga-go-backend/types"
+	"rabotyaga-go-backend/utils"
+)
+
+type Request struct {
+	Event       types.EventType
+	Data        json.RawMessage
+	StartParams *vkapps.Params
+
+	Conn net.Conn
+	Op   ws.OpCode
+}
+
+func (r *Request) SendMessage(e types.EventType, d interface{}) {
+	data, err := utils.MarshalData(e, &d)
+	if err != nil {
+		return
+	}
+
+	err = wsutil.WriteServerMessage(r.Conn, r.Op, data)
+	if err != nil {
+		fmt.Println("[SendMessage] Sending message ended with an error:", err)
+		return
+	}
+}
+
+func (r *Request) SendError(code types.ErrorCode) {
+	r.SendMessage(types.EventError, dto.ResponseError{Code: code})
+}
+
+func (r *Request) Disconnect() {
+	err := r.Conn.Close()
+
+	if err != nil {
+		fmt.Println("[Disconnect] Disconnect user ended with an error:", err)
+	}
+}
+
+func (r *Request) PickInt(k string) *int {
+	var data map[string]interface{}
+
+	err := json.Unmarshal(r.Data, &data)
+	if err != nil {
+		fmt.Println("[Error] Failed to unmarshal JSON:", err)
+		return nil
+	}
+
+	if val, ok := data[k]; ok {
+		if floatVal, ok := val.(float64); ok {
+			intVal := int(floatVal)
+			return &intVal
+		}
+	}
+
+	return nil
+}
